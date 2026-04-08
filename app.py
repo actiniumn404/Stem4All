ed0a1af29ec030b1647c7befca1d4deed14ed66b
actiniumn404 <83673744+actiniumn404@users.noreply.github.com>
2026-04-07 23:02:23 -0700

    Add hero and our mission section

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
 from flask import Flask, render_template, request, jsonify, make_response, redirect
 from flask_frozen import Freezer
 
-import os
-
+import sys
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 freezer = Freezer(app)
 
+
 @app.route('/')
 def page_home():
     return render_template("lander.html")
 
 
-
 if __name__ == "__main__":
-    if "DEV" in os.environ:
+    if "DEBUG" in sys.argv:
         app.run(port=80, host="0.0.0.0")
     else:
         freezer.freeze()
-
